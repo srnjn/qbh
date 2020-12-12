45692a2f68f26ce2fd59ee13fd1f374e562e4ae8
Vishal Srivastava <visriv@microsoft.com>
2020-12-12 17:19:06 +0530

    Adding train nets with simple FC layers

---
+++ networks.py
@@ -0,0 +1,109 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.convnet = nn.Sequential(nn.Conv2d(1, 32, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2),
+                                     nn.Conv2d(32, 64, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2))
+
+        self.fc = nn.Sequential(nn.Linear(64 * 4 * 4, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 2)
+                                )
+
+    def forward(self, x):
+        output = self.convnet(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+class FCNet(nn.Module):
+    def __init__(self, input_size, num_classes):
+      super(FCNet, self).__init__()
+      self.fc = nn.Sequential(nn.Linear(250, 150),
+                                nn.PReLU(),
+                                nn.Linear(150, 100),
+                                nn.PReLU(),
+                                nn.Linear(100, 50)
+                                )
+      self.nonlinear = nn.PReLU()
+      self.last = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+      output = self.fc(x)
+      output = self.nonlinear(output)
+      scores = F.log_softmax(self.last(output), dim=-1)
+      return scores
+
+    def get_embedding(self, x):
+        return self.nonlinear(self.fc(x))
+
+
+
+class EmbeddingNetL2(ConvNet):
+    def __init__(self):
+        super(EmbeddingNetL2, self).__init__()
+
+    def forward(self, x):
+        output = super(EmbeddingNetL2, self).forward(x)
+        output /= output.pow(2).sum(1, keepdim=True).sqrt()
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+
+class ClassificationNet(nn.Module):
+    def __init__(self, embedding_net, embedding_dim, n_classes):
+        super(ClassificationNet, self).__init__()
+        self.embedding_net = embedding_net
+        self.n_classes = n_classes
+        self.nonlinear = nn.PReLU()
+        self.fc1 = nn.Linear(embedding_dim, n_classes)
+
+    def forward(self, x):
+        output = self.embedding_net(x)
+        output = self.nonlinear(output)
+        scores = F.log_softmax(self.fc1(output), dim=-1)
+        return scores
+
+    def get_embedding(self, x):
+        return self.nonlinear(self.embedding_net(x))
+
+
+class SiameseNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(SiameseNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        return output1, output2
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+
+class TripletNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        output3 = self.embedding_net(x3)
+        return output1, output2, output3
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
